41867ce830f08cf87e0856e025ce7312be421fd2
Colin Han <colin@p2m.net.cn>
2017-04-06 18:18:45 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -10,12 +10,12 @@ if [[ "$1" == "-u" ]]; then
 fi
 
 if [[ "$1" == "" || "$1" == "clone" || "$1" == 'c' ]]; then
-    git clone https://github.com/${username}/p2m-message-client.git client/client
-    git clone https://github.com/${username}/p2m-message-client-jpush.git client/jpush
-    git clone https://github.com/${username}/p2m-message-client-socketio.git client/socketio
-    git clone https://github.com/${username}/p2m-message-view-knockout.git client/view/knockout
-    git clone https://github.com/${username}/p2m-message-view-react-native.git client/view/reactnative
-    git clone https://github.com/${username}/p2m-message-server.git server/server
-    git clone https://github.com/${username}/p2m-message-channel-jpush.git server/jpush
+    git clone https://github.com/${username}/p2m-message-client.git client/client && \
+    git clone https://github.com/${username}/p2m-message-client-jpush.git client/jpush && \
+    git clone https://github.com/${username}/p2m-message-client-socketio.git client/socketio && \
+    git clone https://github.com/${username}/p2m-message-view-knockout.git client/view/knockout && \
+    git clone https://github.com/${username}/p2m-message-view-react-native.git client/view/reactnative && \
+    git clone https://github.com/${username}/p2m-message-server.git server/server && \
+    git clone https://github.com/${username}/p2m-message-channel-jpush.git server/jpush && \
     git clone https://github.com/${username}/p2m-message-channel-socket-io.git server/socketio
 fi
