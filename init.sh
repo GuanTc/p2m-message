45a90e9f59b9117d061d09014511597624f2b098
Colin Han <colin@p2m.net.cn>
2017-04-06 18:16:33 +0800

    Fix a mistake.

--- init.sh
+++ init.sh
@@ -12,7 +12,7 @@ fi
 if [[ "$1" == "" || "$1" == "clone" || "$1" == 'c' ]]; then
     git clone https://github.com/${username}/p2m-message-client.git client/client
     git clone https://github.com/${username}/p2m-message-client-jpush.git client/jpush
-    git clone https://github.com/${username}/p2m-message-client-socket-io.git client/socketio
+    git clone https://github.com/${username}/p2m-message-client-socketio.git client/socketio
     git clone https://github.com/${username}/p2m-message-view-knockout.git client/view/knockout
     git clone https://github.com/${username}/p2m-message-view-react-native.git client/view/reactnative
     git clone https://github.com/${username}/p2m-message-server.git server/server
